76b2050d7b77c49160995bc0a6b8fee5e4f2392f
blair wachira <blairwachira@proton.me>
2026-01-29 11:11:42 +0300

    js dice roll

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 function roll_dice() {
   const dices = ["&#9856;", "&#9857;", "&#9858;", "&#9859;", "&#9860;", "&#9861;"];
   let dice_choice = Math.floor(Math.random() * dices.length);
-  document.getElementById("dice_face").innerHTML = dices[dice_choice];
+  const display = document.getElementById("dice_face");
+
+  display.classList.add('roll-animation');
+
+  setTimeout(() => {
+    display.classList.remove("roll-animation");
+    display.innerHTML = dices[dice_choice];
+  }, 200);
 }
